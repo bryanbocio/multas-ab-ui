11ee210052073d10abd752068ea10a96fdbc954c
Albert2707 <albertjohan2707@gmail.com>
2023-06-08 18:29:57 -0400

    Cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,42 @@
 import { useState } from "react";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-  Outlet,
-} from "react-router-dom";
-import Navbar from "./components/navbar/Navbar";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Leftbar from "./components/leftbar/Leftbar";
 import Login from "./pages/login/Login";
 import Home from "./pages/home/Home";
+import Footer from "./components/footer/Footer";
 function App() {
   const Layout = () => {
     return (
-      <div>
-        <Navbar />
-        <Outlet />
-      </div>
+      <>
+        <div className="flex min-h-screen bg-gray-50 ">
+          <Leftbar />
+          <div className="p-5 flex-6">
+            <Outlet />
+          </div>
+        </div>
+
+        <Footer />
+      </>
     );
   };
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Login/>
+      element: <Login />,
     },
     {
-      path:"/home",
-      element: <Layout/>,
-      children:[
+      path: "/home",
+      element: <Layout />,
+      children: [
         {
-          path:"/home",
-          element: <Home/>
-        }
-      ]
-    }
-
+          path: "/home",
+          element: <Home />,
+        },
+      ],
+    },
   ]);
- 
-  return <RouterProvider router={router}/>
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/data.ts
@@ -0,0 +1,40 @@
+interface IData {
+    id: number;
+    tipo: string;
+    desc: string
+}
+
+
+
+
+export const Data: IData[] =[{
+    id:1,
+    tipo:"Exceso de velocidad",
+    desc:"La personas multada iba a a exceso de velociad, especificamente a 150km/h"
+},
+{
+    id:2,
+    tipo:"Exceso de velocidad",
+    desc:"La personas multada iba a a exceso de velociad, especificamente a 150km/h"
+},
+{
+    id:3,
+    tipo:"Exceso de velocidad",
+    desc:"La personas multada iba a a exceso de velociad, especificamente a 150km/h"
+},
+{
+    id:4,
+    tipo:"Exceso de velocidad",
+    desc:"La personas multada iba a a exceso de velociad, especificamente a 150km/h"
+},
+{
+    id:5,
+    tipo:"Exceso de velocidad",
+    desc:"La personas multada iba a a exceso de velociad, especificamente a 150km/h"
+},
+{
+    id:6,
+    tipo:"Exceso de velocidad",
+    desc:"La personas multada iba a a exceso de velociad, especificamente a 150km/h"
+},
+];
