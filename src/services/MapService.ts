8606f49707756c7e0a7772b912738b5faf03380e
Albert2707 <albertjohan2707@gmail.com>
2023-07-06 10:59:08 -0400

    delete mapservice file

--- src/services/MapService.ts
+++
@@ -1,8 +0,0 @@
-import newRequest from "../Request";
-
-export const getTrafficFine = async () => {
-  return await newRequest
-    .get("TrafficFine")
-    .then((results) => results.data.data)
-    .catch((err) => err);
-};
