b501cc110685463c6a07dae8cd9db18b6470e781
Albert2707 <albertjohan2707@gmail.com>
2023-07-06 10:53:27 -0400

    New request axios file

--- src/Request.ts
+++ src/Request.ts
@@ -1,11 +1,14 @@
 import axios from "axios";
+const newRequest = (token:string) => {
+  return axios.create({
+    baseURL: "https://localhost:5001/api/",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
 
-const token = JSON.parse(localStorage.getItem("user")!) || null;
-const newRequest = axios.create({
-  baseURL: "https://localhost:5001/api/",
-  headers: {
-    'Authorization': `Bearer ${token}`,
-  },
-});
+
+
 
 export default newRequest;
