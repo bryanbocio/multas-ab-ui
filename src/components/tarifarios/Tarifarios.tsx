80908fcbd09da549810b5bd8a9c23049a496ad5e
alber <alber@ALBERTSLENOVO>
2023-07-18 14:40:52 -0400

    Adding error component

--- src/components/tarifarios/Tarifarios.tsx
+++ src/components/tarifarios/Tarifarios.tsx
@@ -8,8 +8,10 @@ import { AuthContextType } from "../../context/AuthContextType";
 import Loader from "../loader/Loader";
 import Warnings from "../warnings/Warnings";
 import { hasMultipleRoles } from "../../utils/Roles";
+import ErrorComponent from "../errorComponent/ErrorComponent";
 const Tarifarios = () => {
   const { token, currentUser } = useContext(AuthContext) as AuthContextType;
+  console.log(currentUser.role)
   const role = hasMultipleRoles(currentUser.role);
   const { data, error, isLoading } = useQuery({
     queryKey: ["multas"],
@@ -28,7 +30,9 @@ const Tarifarios = () => {
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {error ? (
-        "Error"
+        <div className="col-span-full mx-auto">
+          <ErrorComponent />
+        </div>
       ) : isLoading ? (
         <Loader />
       ) : data.length ? (
