75daecef9e2eb97262c2a78975a8d7f2be667d46
alber <alber@ALBERTSLENOVO>
2023-08-02 11:53:24 -0400

    CheckoutForm for payments

---
+++ src/components/checkoutForm/CheckoutForm.tsx
@@ -0,0 +1,47 @@
+import {
+  PaymentElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js hasn't yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    const { error } = await stripe.confirmPayment({
+      //`Elements` instance that was used to create the Payment Element
+      elements,
+      confirmParams: {
+        return_url: "http://localhost:5173/home",
+      },
+    });
+
+    if (error) {
+      // Show error to your customer (for example, payment details incomplete)
+      console.log(error.message);
+    } else {
+      // Your customer will be redirected to your `return_url`. For some payment
+      // methods like iDEAL, your customer will be redirected to an intermediate
+      // site first to authorize the payment, then redirected to the `return_url`.
+    }
+  };
+  return (
+    <div className="w-1/2 mx-auto">
+      <form action="" onSubmit={handleSubmit} className="">
+        <PaymentElement />
+        <button disabled={!stripe}>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default CheckoutForm;
