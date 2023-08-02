2b4e9bed11d9076a7458729ddf0726544bec30b8
alber <alber@ALBERTSLENOVO>
2023-08-02 11:52:48 -0400

    Calc total price of trafficFinez

--- src/components/basketItem/BasketItem.tsx
+++ src/components/basketItem/BasketItem.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import newRequest from "../../Request";
 import { AuthContext } from "../../context/authContext";
 import { AuthContextType } from "../../context/AuthContextType";
@@ -30,14 +30,19 @@ const BasketItem: React.FC<Props> = ({ trafficFines, setTotal, total }) => {
         .catch((err) => console.log(err));
     },
   });
-  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.checked) {
-      setTotal(total + parseInt(e.target.value));
-    } else {
-      setTotal(total - parseInt(e.target.value));
-    }
-  };
 
+  useEffect(() => {
+    data &&
+      setTotal(
+        data
+          .filter((e: any) => e.id == trafficFines.trafficFineId)
+          .map((e: any) =>
+            reasonData
+              .filter((reason: any) => reason.reason == e.reason)
+              .map((obj: any) => obj.price)
+          )
+      );
+  }, [data]);
   return (
     <div>
       {error
@@ -51,22 +56,17 @@ const BasketItem: React.FC<Props> = ({ trafficFines, setTotal, total }) => {
                   key="e.id"
                   className="flex gap-5 items-center border-b-[1px] p-2 border-zinc-200 dark:border-zinc-600"
                 >
-                  <input
-                    type="checkbox"
-                    className="hidden w-4 h-4 rounded-lg md:block"
-                    onChange={handleCheckboxChange}
-                    value={reasonData
-                      .filter((reason: any) => reason.reason == e.reason)
-                      .map((obj: any) => obj.price)}
-                  />
                   <div className="flex flex-col max-w-3xl gap-2">
-                    <span className="text-xl font-semibold">{e.reason}</span>
+                    <span className="text-xl font-semibold">
+                      {e.reason.split(" ").slice(1).join(" ")}
+                    </span>
                     <span className="text-lg font-medium">{e.comment}</span>
                     <span className="mt-2 font-medium">
                       RD$
-                      {reasonData
-                        .filter((reason: any) => reason.reason == e.reason)
-                        .map((obj: any) => obj.price)}
+                      {reasonData &&
+                        reasonData
+                          .filter((reason: any) => reason.reason == e.reason)
+                          .map((obj: any) => obj.price)}
                     </span>
                   </div>
                 </div>
