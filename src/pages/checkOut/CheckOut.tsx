e4687c745b4422cdd81e6efe1acc689283d4a834
Albert2707 <albertjohan2707@gmail.com>
2023-08-04 11:59:09 -0400

    adding clientSecret from de basket

--- src/pages/checkOut/CheckOut.tsx
+++ src/pages/checkOut/CheckOut.tsx
@@ -1,16 +1,22 @@
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
-import CheckoutForm from '../../components/checkoutForm/CheckoutForm';
-const stripePromise = loadStripe('pk_test_51NUEcGAzQjuhFyYrMyDy8wwSveZmjBiWFCx83vgQkePnR4mu4hC8UfyBsylz7enDlmYMNUAinfH1qPeV6il4Wh0500hcPSkjhL');
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "../../components/checkoutForm/CheckoutForm";
+import { useLocation } from "react-router-dom";
+const stripePromise = loadStripe(
+  "pk_test_51NUEcGAzQjuhFyYrMyDy8wwSveZmjBiWFCx83vgQkePnR4mu4hC8UfyBsylz7enDlmYMNUAinfH1qPeV6il4Wh0500hcPSkjhL"
+);
 const CheckOut = () => {
+  const { pathname } = useLocation();
+  const client = pathname.split("/")[2];
   const options = {
     // passing the client secret obtained from the server
-    clientSecret: 'pi_3NZ4h6AzQjuhFyYr140q8cIn_secret_s4BIQT8otJqWqY9sdWEf81osr'
+    clientSecret: client,
   };
   return (
-    <Elements stripe={stripePromise}  options={options} >
-      <CheckoutForm  />
-    </Elements>  )
-}
+    <Elements stripe={stripePromise} options={options}>
+      <CheckoutForm />
+    </Elements>
+  );
+};
 
-export default CheckOut
+export default CheckOut;
