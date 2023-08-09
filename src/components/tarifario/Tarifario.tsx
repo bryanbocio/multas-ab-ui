1f3c1a8428339462d044a3eb491e115f26404dc7
Albert2707 <albertjohan2707@gmail.com>
2023-08-09 09:48:00 -0400

    removing dateDistance

--- src/components/tarifario/Tarifario.tsx
+++ src/components/tarifario/Tarifario.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import { Multas } from "../../utils/type";
 import { Link } from "react-router-dom";
-import { formatDistanceToNow } from "date-fns";
-import esLocale from "date-fns/locale/es"; // Importa el locale español
+
 interface Props {
   multa: Multas;
 }
 const Tarifario: React.FC<Props> = ({ multa }) => {
-  const date = new Date(multa.dateCreated);
-  const formattedDistance = formatDistanceToNow(date , { locale: esLocale });
   return (
-    <div className="w-full h-[200px]  rounded-lg p-4 border-[1px] border-gray-200 bg-white dark:bg-[#444] flex flex-col gap-2 dark:border-[#444] justify-between">
-      <span className="text-sm font-semibold capitalize">{formattedDistance}</span>
+    <div className="w-full h-[150px]  rounded-lg p-4 border-[1px] border-gray-200 bg-white dark:bg-[#444] flex flex-col gap-2 dark:border-[#444] justify-between">
       <span className="text-lg font-bold dark:text-[whitesmoke] truncate">
         {multa.reason.split(" ").slice(1).join(" ")}
       </span>
