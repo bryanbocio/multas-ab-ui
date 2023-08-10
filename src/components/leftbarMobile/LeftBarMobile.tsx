ba7ba9abae4533179fe5370de14d1b2c926ade02
Albert2707 <albertjohan2707@gmail.com>
2023-08-10 13:38:22 -0400

    leftbar

--- src/components/leftbarMobile/LeftBarMobile.tsx
+++ src/components/leftbarMobile/LeftBarMobile.tsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/authContext";
 import { AuthContextType } from "../../context/AuthContextType";
 import { hasMultipleRoles } from "../../utils/Roles";
@@ -8,9 +8,14 @@ interface Props {
   setShowMobile: React.Dispatch<React.SetStateAction<boolean>>;
 }
 const LeftBarMobile: React.FC<Props> = ({ setShowMobile }) => {
-  const { currentUser } = useContext(AuthContext) as AuthContextType;
+  const { currentUser, logout } = useContext(AuthContext) as AuthContextType;
   const role = hasMultipleRoles(currentUser.role);
+  const navigate = useNavigate();
   const { pathname } = useLocation();
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
   return (
     <div className="fixed  inset-x-0 top-[77px] bottom-[72px] overflow-scroll block md:hidden z-10  bg-[#f6f3f3] dark:bg-[#444]">
       <div className="flex flex-col gap-2 pt-5">
@@ -26,6 +31,11 @@ const LeftBarMobile: React.FC<Props> = ({ setShowMobile }) => {
           >
             Tarifario de multas
           </Link>
+        </div>
+        <div
+          className="dark:text-[lightgray] hover:bg-gray-200 dark:hover:bg-[#333] p-3 "
+          onClick={() => setShowMobile(false)}
+        >
           <Link
             to="/record"
             className={`text-xl font-medium ${
@@ -35,6 +45,7 @@ const LeftBarMobile: React.FC<Props> = ({ setShowMobile }) => {
             Historico de multas
           </Link>
         </div>
+
         {role === "ADMIN" && (
           <div
             className="dark:text-[lightgray] hover:bg-gray-200 dark:hover:bg-[#333] p-3 "
@@ -50,7 +61,7 @@ const LeftBarMobile: React.FC<Props> = ({ setShowMobile }) => {
             </Link>
           </div>
         )}
-        {(role == "ADMIN") && (
+        {role == "ADMIN" && (
           <div
             className="dark:text-[lightgray] hover:bg-gray-200 dark:hover:bg-[#333] p-3 "
             onClick={() => setShowMobile(false)}
@@ -121,6 +132,14 @@ const LeftBarMobile: React.FC<Props> = ({ setShowMobile }) => {
           >
             Estado del Clima
           </Link>
+        </div>
+        <div className="dark:text-[lightgray] hover:bg-gray-200 dark:hover:bg-[#333] p-3 ">
+          <button
+            onClick={handleLogout}
+            className="text-xl font-medium text-rose-500"
+          >
+            Cerrar sesion
+          </button>
         </div>
       </div>
     </div>
