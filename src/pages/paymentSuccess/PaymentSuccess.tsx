beb754210a1aad7eaa1def543846f664d078c747
Albert2707 <albertjohan2707@gmail.com>
2023-08-09 22:53:34 -0400

    Payment

--- src/pages/paymentSuccess/PaymentSuccess.tsx
+++ src/pages/paymentSuccess/PaymentSuccess.tsx
@@ -4,14 +4,20 @@ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/authContext";
 import { AuthContextType } from "../../context/AuthContextType";
 import newRequest from "../../Request";
+import Invoice from "../../components/invoice/Invoice";
 
 const PaymentSuccess = () => {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { token, currentUser } = useContext(AuthContext) as AuthContextType;
-  const { mutate } = useMutation({
+  const { mutateAsync, data } = useMutation({
     mutationFn: (params: { basketId: string; driverIdentity: string }) => {
-      return newRequest(token).post("Order", params);
+      return newRequest(token)
+        .post("Order", params)
+        .then((result) => {
+          return result.data;
+        })
+        .catch((err) => console.log(err));
     },
     onSuccess: () => {
       queryClient.invalidateQueries(["basketItem"]),
@@ -20,11 +26,13 @@ const PaymentSuccess = () => {
   });
 
   useEffect(() => {
-    mutate({
+    const datos = {
       basketId: currentUser.given_name,
       driverIdentity: currentUser.given_name,
-    });
-  }, []);
+    };
+    mutateAsync(datos);
+  }, [currentUser.given_name, mutateAsync]);
+
   return (
     <div className="container mx-auto">
       <div className="flex flex-col items-center justify-center w-full gap-2 p-10 font-bold bg-white rounded-lg dark:bg-[#444] dark:border-[#333] border-[1px] border-gray-200">
@@ -55,6 +63,7 @@ const PaymentSuccess = () => {
           Volver
         </button>
       </div>
+      {data && <Invoice multa={data} />}
     </div>
   );
 };
