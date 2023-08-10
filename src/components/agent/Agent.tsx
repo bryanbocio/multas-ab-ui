47b5cbaede843c870d5e46af6f18d3eabd2a327f
Albert2707 <albertjohan2707@gmail.com>
2023-08-10 10:52:05 -0400

    Format

--- src/components/agent/Agent.tsx
+++ src/components/agent/Agent.tsx
@@ -1,19 +1,21 @@
-import { AgentType } from '../../utils/type'
+import { AgentType } from "../../utils/type";
 
-interface Props{
-    agent: AgentType
+interface Props {
+  agent: AgentType;
 }
 
-const Agent:React.FC<Props> = ({agent}) => {
+const Agent: React.FC<Props> = ({ agent }) => {
   return (
     <div className="w-full rounded-lg p-4 border-[1px] border-gray-200 bg-white dark:bg-[#444] dark:text-[whitesmoke]  flex flex-col gap-2 dark:border-[#444]">
-      <h2 className="font-semibold text-xl">
+      <h2 className="text-xl font-semibold">
         {agent.name} {agent.lastName}
       </h2>
       <span className="text-xl font-normal ">{agent.phoneNumber}</span>
-      <span className="text-xl font-normal ">{agent.agentIdentity}</span>
+      <span className="text-xl font-normal ">
+        {agent.agentIdentity.replace(/(\d{3})(\d{7})(\d{1})/, "$1-$2-$3")}
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default Agent
+export default Agent;
