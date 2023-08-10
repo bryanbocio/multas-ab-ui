d43ff2ff48a98662edc714bd61264e5f974c02dc
Albert2707 <albertjohan2707@gmail.com>
2023-08-10 13:39:34 -0400

    Format ID and phoneNumber

--- src/utils/formatIdentityId.ts
+++ src/utils/formatIdentityId.ts
@@ -14,3 +14,18 @@ export const format = (event: any): string => {
 
   return formattedInputValue;
 };
+
+export const formatPhoneNumber = (inputNumber: string) => {
+  const cleanedNumber = inputNumber.replace(/\D/g, "");
+  const formattedNumber = cleanedNumber.replace(
+    /(\d{0,3})(\d{0,3})(\d{0,4})/,
+    (match, p1, p2, p3) => {
+      let formatted = "";
+      if (p1) formatted += `(${p1}`;
+      if (p2) formatted += `) ${p2}`;
+      if (p3) formatted += `-${p3}`;
+      return formatted;
+    }
+  );
+  return formattedNumber;
+};
